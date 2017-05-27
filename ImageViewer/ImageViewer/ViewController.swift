91cd928cd742060facee11a94d72799345cd1a1e
byunhyeongwon <bhw0506@gmail.com>
2017-05-27 20:59:15 +0900

    Image Viewer : complete

--- ImageViewer/ImageViewer/ViewController.swift
+++ ImageViewer/ImageViewer/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var imageName = ["Cosmos01.jpg", "Cosmos02.jpg", "Cosmos03.jpg"]
+    var state = 0
+    var img : UIImage?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView.image = UIImage(named: imageName[state])
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func prevBtn(_ sender: UIButton) {
+        
+        state += 1
+        
+        if(state == 3) {
+            
+            state = 0;
+        }
+            
+        imageView.image = UIImage(named: imageName[state])
+        
+    }
+    
+    @IBAction func nextBtn(_ sender: UIButton) {
+        
+        state -= 1
+        
+        if(state == -1) {
+            
+            state = 2;
+        }
+        
+        imageView.image = UIImage(named: imageName[state])
+        
+    }
+    
 
 }
 
