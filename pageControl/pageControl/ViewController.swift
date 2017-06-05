646fcc24dc2e89e9b8f16025ec8e64a099f6f699
byunhyeongwon <bhw0506@gmail.com>
2017-06-05 16:47:48 +0900

    Add pageControl & Mission_09

--- pageControl/pageControl/ViewController.swift
+++ pageControl/pageControl/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+var images = [ "01.png", "02.png", "03.png", "04.png", "05.png", "06.png",]
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        pageControl.numberOfPages = images.count;
+        pageControl.currentPage = 0;
+        pageControl.pageIndicatorTintColor = UIColor.green;
+        pageControl.currentPageIndicatorTintColor = UIColor.red;
+        imgView.image = UIImage(named: images[0]);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pageChanged(_ sender: UIPageControl) {
+        
+        imgView.image = UIImage(named: images[pageControl.currentPage]);
+    }
 
 }
 
